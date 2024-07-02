116467f1b70de3c1f81bd6cec6aadd5c4ce03971
hyamaguchi <hyamaguchi@xcoo.jp>
2024-07-02 23:43:26 +0900

    FIB
    FIB

---
+++ src/fib.clj
@@ -0,0 +1,17 @@
+(ns fib
+  (:require [core :refer [rosalind-solve]]))
+
+;; f_0 = 0
+;; f_1 = 1
+;; f_{n} = k * f_{n - 1} + f_{n - 2}
+
+(defn fib
+  [xs]
+  (let [[n k] (map #(Long/parseLong %) (clojure.string/split xs #"\s"))]
+    (->> [0 1]
+         (iterate (fn [[a b]] [(+ a b) (* k a)]))
+         (take (inc n))
+         last
+         first)))
+
+(rosalind-solve)
